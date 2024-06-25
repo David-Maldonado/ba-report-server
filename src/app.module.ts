24c902559b41c91ff185c3434225685c5acbafbf
David Maldonado <david_maldg@hotmail.com>
2024-06-25 19:40:45 -0400

    primer commit - config

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+
+
+import { BasicReportsModule } from './basic-reports/basic-reports.module';
+
+@Module({
+  imports: [BasicReportsModule
+  ],
+})
+export class AppModule {}
