7bf3eda478ee33f906874e2dc0c97412e7d0374f
David Maldonado <david_maldg@hotmail.com>
2024-07-01 17:35:06 -0400

    reporte con graficos y svgs

--- src/store-reports/store-reports.service.ts
+++ src/store-reports/store-reports.service.ts
@@ -1,49 +1,82 @@
 import { Injectable, NotFoundException, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { PrinterService } from 'src/printer/printer.service';
-import { orderByIdReport } from 'src/reports';
+import {
+  getBasicChartSvgReport,
+  getStatisticsReport,
+  orderByIdReport,
+} from 'src/reports';
 
 @Injectable()
 export class StoreReportsService extends PrismaClient implements OnModuleInit {
-   async onModuleInit() {
-        await this.$connect()
-    }
-    constructor(
-        private printeService: PrinterService
-    ){
-        super();
-    }
- 
-   async getOrderReportByOrderId(orderId:number){
-        
+  async onModuleInit() {
+    await this.$connect();
+    // console.log('Connected to the database');
+  }
 
-     const order = await this.orders.findUnique({
-        where: {
-            order_id: orderId
-          },
-          //"join" con prisma
+  constructor(private readonly printerService: PrinterService) {
+    super();
+  }
+
+  async getOrderByIdReport(orderId: number) {
+    const order = await this.orders.findUnique({
+      where: {
+        order_id: orderId,
+      },
+      include: {
+        customers: true,
+        order_details: {
           include: {
-            customers:true,
-            order_details:{
-                include: {
-                    products:true
-                }
-            }
-          }
-     })
+            products: true,
+          },
+        },
+      },
+    });
 
-     if(!order){
-        throw new NotFoundException(`Order with id ${orderId} not found`)
-     }
-
-
-        const docDefinition = orderByIdReport({
-            data:order as any
-        })
-
-        const doc = this.printeService.createPdf(docDefinition)
-
-        return doc;
+    if (!order) {
+      throw new NotFoundException(`Order with id ${orderId} not found`);
     }
 
+    const docDefinition = orderByIdReport({
+      data: order as any,
+    });
+
+    const doc = this.printerService.createPdf(docDefinition);
+
+    return doc;
+  }
+
+  async getSvgChart() {
+    const docDefinition = await getBasicChartSvgReport();
+
+    const doc = this.printerService.createPdf(docDefinition);
+
+    return doc;
+  }
+
+  async getStatistics() {
+    const topCountries = await this.customers.groupBy({
+      by: ['country'],
+      _count: true,
+      orderBy: {
+        _count: {
+          country: 'desc',
+        },
+      },
+      take: 10,
+    });
+
+    const topCountryData = topCountries.map(({ country, _count }) => ({
+      country: country,
+      customers: _count,
+    }));
+
+    const docDefinition = await getStatisticsReport({
+      topCountries: topCountryData,
+    });
+
+    const doc = this.printerService.createPdf(docDefinition);
+
+    return doc;
+  }
 }
